bc7e6fff075d3f5f592087eb35bf2aff547b9869
Sebastian Banaś <sebastianbanas4@gmail.com>
2022-11-14 12:06:42 +0100

    add DataGenerator dependency on index.php

--- App/Classes/Datagenerator.php
+++ App/Classes/Datagenerator.php
@@ -1,6 +1,9 @@
 <?php
+    require('..\..\index.php')
+    
     class DataGenerator
     {
-
+        $number = (int) $_POST["number"];
+        $isLeapYear = (bool) (isset($_POST["leapyear"]) ? true : false);
     }
 ?>
