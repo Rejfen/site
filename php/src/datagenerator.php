b91d181c1b1c4a5dd092103fc122867a993b84e1
Sebastian Banaś <sebastianbanas4@gmail.com>
2022-11-17 13:55:06 +0100

    separated Data Generation from index.php

---
+++ php/src/datagenerator.php
@@ -0,0 +1,70 @@
+<?php
+    include(__DIR__ . '/App/Classes/Datagenerator.php');
+
+    if (!empty($_POST) && array_key_exists('number', $_POST)) {
+        $number = (int) $_POST["number"];
+        $isLeapYear = (bool) (isset($_POST["leapyear"]) ? true : false);
+
+        $list = new DataGenerator($isLeapYear);
+        $error = $list->validateData($number);
+        $results = $list->checkLeapYearAndGetAllDates($number);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <title>Generator dat</title>
+        <link rel="stylesheet" href="style.css">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+        include('visual/menu.html');
+        ?>
+        <div>
+            <?php 
+            include('visual/sidebarleft.html');
+            ?>
+                <div class="workspc">
+                    <br /><br /><br /><br /><br /><hr /><br />
+                    <div class="formmod">
+                        <br />
+                        <form  method="POST" class="textmod">
+                            <label for="number">Podaj liczbę</label>
+                            <input type="number" name="number" id="number">
+                            <br /><br />
+                            <label for="leapyear">Uwzględnij dni przestępne</label>
+                            <input type="checkbox" name="leapyear" id="leapyear">
+                            <br /><br />
+                            <input type="submit" name="submit" value="Wyślij">
+                        </form>
+                        <br />
+                        <br />
+                        <div class="list">
+                            <?php
+                                if (isset($number) == true) {
+                                    if ($error !== '') {
+                                        echo "<div class=textmod>".$error."</div>"; 
+                                    } 
+                                    else {
+                                        echo "<ul class=textmod>";
+
+                                        foreach ($results as $result) {
+                                            echo $result;
+                                        }
+
+                                        echo "</ul>";
+                                    }
+                                }
+                            ?>
+                        </div>
+                    </div>
+                    <br /><br /><hr />
+                </div>
+            <?php 
+            include('visual/sidebarRight.html');
+            ?>
+        </div>
+    </body>
+</html>
--- php/src/index.php
+++ php/src/index.php
@@ -1,20 +1,7 @@
-<?php
-    include(__DIR__ . '/App/Classes/Datagenerator.php');
-
-    if (!empty($_POST) && array_key_exists('number', $_POST)) {
-        $number = (int) $_POST["number"];
-        $isLeapYear = (bool) (isset($_POST["leapyear"]) ? true : false);
-
-        $list = new DataGenerator($isLeapYear);
-        $error = $list->validateData($number);
-        $results = $list->checkLeapYearAndGetAllDates($number);
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="pl">
     <head>
-        <title>Strona</title>
+        <title>Strona Główna</title>
         <link rel="stylesheet" href="style.css">
         <meta charset="UTF-8">
     </head>
@@ -28,37 +15,10 @@
             ?>
                 <div class="workspc">
                     <br /><br /><br /><br /><br /><hr /><br />
-                    <div class="formmod">
-                        <br />
-                        <form  method="POST" class="textmod">
-                            <label for="number">Podaj liczbę</label>
-                            <input type="number" name="number" id="number">
-                            <br /><br />
-                            <label for="leapyear">Uwzględnij dni przestępne</label>
-                            <input type="checkbox" name="leapyear" id="leapyear">
-                            <br /><br />
-                            <input type="submit" name="submit" value="Wyślij">
-                        </form>
-                        <br />
-                        <br />
-                        <div class="list">
-                            <?php
-                                if (isset($number) == true) {
-                                    if ($error !== '') {
-                                        echo "<div class=textmod>".$error."</div>"; 
-                                    } 
-                                    else {
-                                        echo "<ul class=textmod>";
-
-                                        foreach ($results as $result) {
-                                            echo $result;
-                                        }
-
-                                        echo "</ul>";
-                                    }
-                                }
-                            ?>
-                        </div>
+                    <div class="textmod">
+                        Wybierz funkcję
+                        <br /><br />
+                        <img src="visual/smile.png" alt="Smile">
                     </div>
                     <br /><br /><hr />
                 </div>
