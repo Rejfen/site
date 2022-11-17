ed02fd9b8f7784f0be77da8baae1613f455d264e
Sebastian Banaś <sebastianbanas4@gmail.com>
2022-11-17 14:07:58 +0100

    add site for perfect number gen

---
+++ php/src/perfectnumbercheck.php
@@ -0,0 +1,43 @@
+<?php
+    include(__DIR__ . '/App/Classes/');
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <title>Sprawdzacz liczb doskonałych</title>
+        <link rel="stylesheet" href="style.css">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+        include('visual/menu.html');
+        ?>
+        <div>
+            <?php 
+            include('visual/sidebarleft.html');
+            ?>
+                <div class="workspc">
+                    <br /><br /><br /><br /><br /><hr /><br />
+                    <div class="formmod">
+                        <br />
+                        <form  method="POST" class="textmod">
+                            <label for="number">Podaj liczbę "perfekcyjną"</label>
+                            <input type="number" name="number" id="number">
+                            <br /><br />
+                            <input type="submit" name="submit" value="Wyślij">
+                        </form>
+                        <br />
+                        <br />
+                        
+                    </div>
+                    <br /><br /><hr />
+                </div>
+            <?php 
+            include('visual/sidebarRight.html');
+            ?>
+        </div>
+    </body>
+</html>
